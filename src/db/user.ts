c40fa0855b863b02e7f22212e1737571b21205d2
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-05-30 08:52:15 -0400

    md5 hashing (#3)

--- src/db/user.ts
+++ src/db/user.ts
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-import encrypt from 'mongoose-encryption';
 
 export const userConnection = mongoose.createConnection('mongodb://127.0.0.1:27017/userDB');
 
@@ -7,10 +6,5 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-
-const ENCRYPTION_KEY = process.env.ENCKEY;
-const SIGNING_KEY = process.env.SIGKEY;
-
-userSchema.plugin(encrypt, { encryptionKey: ENCRYPTION_KEY, signingKey: SIGNING_KEY, encryptedFields: ['password'] });
 
 export const User = userConnection.model('User', userSchema);
