6b38d0e157ff0e86773cc8a9fd7a01a08be6d5e4
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-05-30 11:15:15 -0400

    refactor md5 logic

--- src/db/user.ts
+++ src/db/user.ts
@@ -1,10 +1,20 @@
 import mongoose from 'mongoose';
+import md5 from 'md5';
 
-export const userConnection = mongoose.createConnection('mongodb://127.0.0.1:27017/userDB');
+const userConnection = mongoose.createConnection('mongodb://127.0.0.1:27017/userDB');
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
 
-export const User = userConnection.model('User', userSchema);
+const User = userConnection.model('User', userSchema);
+
+export const saveUser = async (user: { email: string; password: string }) => {
+  const newUser = new User({ email: user.email, password: md5(user.password) });
+  return await newUser.save().catch((err) => console.log(err));
+};
+
+export const findUser = async (user: { email: string; password: string }) => {
+  return await User.findOne({ email: user.email, password: md5(user.password) }).catch((err) => console.log(err));
+};
